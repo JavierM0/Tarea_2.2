15e388196a0915d22609ed8ccab861950881d893
Javi <a335978@uach.mx>
2021-11-16 21:02:02 -0700

    5.js

---
+++ 5.js
@@ -0,0 +1,9 @@
+var cont = 0;
+for (var i = 1; i < 101; i++){
+    var n = i;
+    if (n%2!=0){
+      console.log(n); 
+        cont++;
+    }
+}
+console.log("el total de numeros impares es: " + cont);
